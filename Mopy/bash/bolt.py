3e113c4ccb8ae588c16c35b3f8f41c54f8d352ba
MrD <the.ubik@gmail.com>
2016-01-23 23:30:32 +0100

    Using scandir:
    Bash will now detect the presence of the scandir module and use its
    walk method.
    
    https://pypi.python.org/pypi/scandir/1.2

--- Mopy/bash/bolt.py
+++ Mopy/bash/bolt.py
@@ -55,6 +55,14 @@ if os.name == u'nt':
     startupinfo = subprocess.STARTUPINFO()
     startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
+# speed up os.walk
+try:
+    import scandir
+    _walk = walkdir = scandir.walk
+except ImportError:
+    _walk = walkdir = os.walk
+    scandir = None
+
 # Unicode ---------------------------------------------------------------------
 #--decode unicode strings
 #  This is only useful when reading fields from mods, as the encoding is not
@@ -728,7 +736,7 @@ class Path(object):
             join = os.path.join
             getSize = os.path.getsize
             try:
-                return sum([sum(map(getSize,map(lambda z: join(x,z),files))) for x,y,files in os.walk(self._s)])
+                return sum([sum(map(getSize,map(lambda z: join(x,z),files))) for x,y,files in _walk(self._s)])
             except ValueError:
                 return 0
         else:
@@ -814,10 +822,10 @@ class Path(object):
         """Like os.walk."""
         if relative:
             start = len(self._s)
-            for root_dir,dirs,files in os.walk(self._s,topdown,onerror):
+            for root_dir,dirs,files in _walk(self._s,topdown,onerror):
                 yield (GPath(root_dir[start:]),[GPath(x) for x in dirs],[GPath(x) for x in files])
         else:
-            for root_dir,dirs,files in os.walk(self._s,topdown,onerror):
+            for root_dir,dirs,files in _walk(self._s,topdown,onerror):
                 yield (GPath(root_dir),[GPath(x) for x in dirs],[GPath(x) for x in files])
 
     def split(self):
@@ -873,7 +881,7 @@ class Path(object):
             except UnicodeError:
                 flags = stat.S_IWUSR|stat.S_IWOTH
                 chmod = os.chmod
-                for root_dir,dirs,files in os.walk(self._s):
+                for root_dir,dirs,files in _walk(self._s):
                     rootJoin = root_dir.join
                     for directory in dirs:
                         try: chmod(rootJoin(directory),flags)
