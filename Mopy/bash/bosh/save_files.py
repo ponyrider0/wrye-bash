a51034bc56fc1cc16543b5ecb36e0fdbc71470f8
nycz <pyntix@gmail.com>
2017-06-23 12:35:35 +0200

    Comments and minor fixes to SSE saves LZ4 decompression
    More comments, some typos fixed, and also the LSIC function won't
    arbitrarily stop after 20 bytes now.

--- Mopy/bash/bosh/save_files.py
+++ Mopy/bash/bosh/save_files.py
@@ -120,49 +120,6 @@ class SaveFileHeader(object):
                 u'Save game masters size (%i) not as expected (%i).' % (
                     ins.tell() - self._mastersStart - 4, mastersSize))
 
-    def _decompress_masters_sse(self, ins): # for skyrim special edition
-        """Read the start of the LZ4 compressed data in the SSE savefile and
-        stop when the whole master table is found.
-        Return a file-like object that can be read by _load_masters_16
-        containing the now decompressed master table."""
-        def _read_lisc_int():
-            result = 0
-            for _ in xrange(20):
-                num = unpack_byte(ins)
-                result += num
-                if num != 255:
-                    return result
-        ins.seek(8, 1)  # skip uncompressed/compressed size for now
-        self._mastersStart = ins.tell()
-        uncompressed = ''
-        mastersSize = None
-        while True:  # parse and decompress each block here
-            token = unpack_byte(ins)
-            literal_length = token >> 4
-            if literal_length == 15:
-                literal_length += _read_lisc_int()
-            uncompressed += ins.read(literal_length)
-            offset = unpack_short(ins)
-            match_length = token & 0b1111
-            if match_length == 15:
-                match_length += _read_lisc_int()
-            match_length += 4
-            start_pos = len(uncompressed) - offset
-            end_pos = start_pos + match_length
-            while start_pos < end_pos: # matches can be overlapping
-                uncompressed += uncompressed[start_pos:min(start_pos + offset,
-                                                           end_pos)]
-                start_pos += offset
-            # the target size is found in bytes 1-5
-            if mastersSize is None and len(uncompressed) >= 5:
-                mastersSize = struct.unpack('I', uncompressed[1:5])[0]
-            # no need to go on after we hit the size of the table
-            if mastersSize is not None:
-                if len(uncompressed) >= mastersSize + 5:
-                    break
-        # wrap the decompressed data in a file-like object and return it
-        return StringIO.StringIO(uncompressed[1:])
-
     def calc_time(self): pass
 
     @property
@@ -276,6 +233,67 @@ class SkyrimSaveHeader(SaveFileHeader):
             ins.read(1) # drop unknown byte
             #--Masters
             self._load_masters_16(ins)
+
+    def _decompress_masters_sse(self, ins):
+        """Read the start of the LZ4 compressed data in the SSE savefile and
+        stop when the whole master table is found.
+        Return a file-like object that can be read by _load_masters_16
+        containing the now decompressed master table.
+        See https://fastcompression.blogspot.se/2011/05/lz4-explained.html
+        for an LZ4 explanation/specification."""
+        def _read_lsic_int():
+            # type: () -> int
+            """Read a compressed int from the stream.
+            In short, add every byte to the output until a byte lower than
+            255 is found, then add that as well and return the total sum.
+            LSIC stands for linear small-integer code, taken from
+            https://ticki.github.io/blog/how-lz4-works."""
+            result = 0
+            while True:  # there is no size limit to LSIC values
+                num = unpack_byte(ins)
+                result += num
+                if num != 255:
+                    return result
+        # Skip decompressed/compressed size, we only want the masters table
+        ins.seek(8, 1)
+        self._mastersStart = ins.tell()
+        uncompressed = ''
+        masters_size = None  # type: int
+        while True:  # parse and decompress each block here
+            token = unpack_byte(ins)
+            # How many bytes long is the literals-field?
+            literal_length = token >> 4
+            if literal_length == 15:  # add more if we hit max value
+                literal_length += _read_lsic_int()
+            # Read all the literals (which are good ol' uncompressed bytes)
+            uncompressed += ins.read(literal_length)
+            # The offset is how many bytes back in the uncompressed string the
+            # start of the match-field (copied bytes) is
+            offset = unpack_short(ins)
+            # How many bytes long is the match-field?
+            match_length = token & 0b1111
+            if match_length == 15:
+                match_length += _read_lsic_int()
+            match_length += 4  # the match-field always gets an extra 4 bytes
+            # The boundary of the match-field
+            start_pos = len(uncompressed) - offset
+            end_pos = start_pos + match_length
+            # Matches can be overlapping (aka including not yet decompressed
+            # data) so we can't jump the whole match_length directly
+            while start_pos < end_pos:
+                uncompressed += uncompressed[start_pos:min(start_pos + offset,
+                                                           end_pos)]
+                start_pos += offset
+            # The masters table's size is found in bytes 1-5
+            if masters_size is None and len(uncompressed) >= 5:
+                masters_size = struct.unpack('I', uncompressed[1:5])[0]
+            # Stop when we have the whole masters table
+            if masters_size is not None:
+                if len(uncompressed) >= masters_size + 5:
+                    break
+        # Wrap the decompressed data in a file-like object and return it,
+        # skipping the first byte since the master size starts at the second
+        return StringIO.StringIO(uncompressed[1:])
 
     def calc_time(self):
         # gameDate format: hours.minutes.seconds
